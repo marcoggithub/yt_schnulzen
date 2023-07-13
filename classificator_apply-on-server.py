d4a9b89f04b6b84c40f711d55e98e5de80e8504f
Q-Bukold <83951996+Q-Bukold@users.noreply.github.com>
2023-07-13 15:05:05 +0200

    better classficator with probabilites

--- classificator_apply-on-server.py
+++ classificator_apply-on-server.py
@@ -3,6 +3,8 @@ import sys
 #machine learning packages
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 # https://pypi.org/project/simpletransformers/ <- source evaluation tipps
+from scipy.special import softmax
+#https://github.com/ThilinaRajapakse/simpletransformers/issues/30 
 import pandas as pd
 from sys import argv
 from sys import stderr
@@ -18,7 +20,7 @@ def predict_comments(filename, modelname):
     # input has to be list
     apply_df = pd.read_csv(filename, sep="\t")
     apply_lst = apply_df['comment'].tolist()
-    apply_lst = apply_lst[:5]
+    apply_lst = apply_lst[:1]
     print("the application list consists of {} elements".format(len(apply_lst)))
     
     # specify model used
@@ -39,62 +41,13 @@ def predict_comments(filename, modelname):
 
     return output
 
+if __name__ == '__main__':
+    output = predict_comments("comment_downloads/data_clean_all.tsv", "model_p82")
 
-output = predict_comments("comment_downloads/data_combined_53221_V2.tsv", "model_p82")
-print(output)
-
-
-
-
-'''
-
-
-
-
-
-#list_of_filters = [["Ed_Sheeran-Thinking_Out_Loud", True], ["Tom_Odell-Another_Love", True], ["Kelly_Clarkson-Because_Of_You", True], ["Bruno_Mars-When_I_Was_Your_Man", True], ["John_Legend-All_of_Me", False]]
-
-
-# load model
-model = ClassificationModel('distilbert','model_p82', use_cuda=False)
-#apply model
-
-com_lst = []
-pred_lst = []
-
-for i, comment in enumerate(apply_lst):
-    single_lst = []
-    single_lst.append(comment)
-    comment = single_lst
-    print(i)
-    predictions, raw_outputs = model.predict(comment)
-    com_lst.append(comment)
-    pred_lst.append(predictions)
-
-print(20*"-")
-print(filename, origin, time_stamp)
-
-# output and save predictions
-final = []
-for i, comment in enumerate(com_lst):
-  comment_row = (comment, str(pred_lst[i]))
-  final.append(comment_row)
-
-df = pd.DataFrame(final, columns =['txt', 'prediction'])
-
-print("1 = schnulzig; 0 = nicht-schnulzig")
-counts = df["prediction"].value_counts()
-print(counts)
-try:
-    perc = counts.iloc[1]/(counts.iloc[0]+counts.iloc[1])
-except:
-    perc = 0
-perc = '{:,.2%}'.format(perc)
-print(f"{perc} der Kommentare wurden als schnulzig klassifiziert.")
-
-
-filename = 'output.tsv'
-df.to_csv(filename, sep='\t')
-print(f"results saved in {filename}")
-
-'''
+    tidy_output = []
+    for comments, prediction, raw_outputs in output:
+        #softmax to get probablities from raw_outputs
+        tidy_output.append([comments, prediction[0], softmax(raw_outputs, axis=1)[0][1]])
+    
+    df = pd.DataFrame(tidy_output, columns=["comments", "prediction", "probability_class_one"])
+    df.to_csv("classification_all.tsv", sep="\t")
