e9ce08db64e63c9e7896045873ffed5783f8a102
Q-Bukold <bukold@uni-hildesheim.de>
2023-07-21 18:45:24 +0200

    back from server

--- BERT-Classification/classification_functions/train_functions.py
+++ BERT-Classification/classification_functions/train_functions.py
@@ -1,39 +1,43 @@
 # encoding=utf-8
 ### All my Simpletransformers apply functions
 # pip install simpletransformers==0.63.9
-# split_data() 
+# split_data()
 # train_model()
 # eval_model()
 
 
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
+
 # https://pypi.org/project/simpletransformers/ <- source evaluation tipps
 from scipy.special import softmax
-#https://github.com/ThilinaRajapakse/simpletransformers/issues/30 
+
+# https://github.com/ThilinaRajapakse/simpletransformers/issues/30
 import pandas as pd
 from sys import argv
 from sys import stderr
 import os
 import logging
-#logging.basicConfig(level=logging.INFO)
-#transformers_logger = logging.getLogger("transformers")
-#transformers_logger.setLevel(logging.INFO)
+
+# logging.basicConfig(level=logging.INFO)
+# transformers_logger = logging.getLogger("transformers")
+# transformers_logger.setLevel(logging.INFO)
 
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+
 # split annotation dataset
-def split_data(annotations, text_columnname : str, categories_columnname : str):
+def split_data(annotations, text_columnname: str, categories_columnname: str):
     print("splitting annotation dataset")
-    #shuffle data and drop old index
+    # shuffle data and drop old index
     annotations = annotations.sample(frac=1, random_state=1).reset_index(drop=True)
 
     # create training-data <-- df
-    train_df = annotations.head(int(len(annotations)*0.8))
+    train_df = annotations.head(int(len(annotations) * 0.8))
     selected_columns = [text_columnname, categories_columnname]
     train_df = train_df[selected_columns]
 
     # create eval-data <-- df
-    eval_df = annotations.tail(int(len(annotations)*0.2))
+    eval_df = annotations.tail(int(len(annotations) * 0.2))
     selected_columns = [text_columnname, categories_columnname]
     eval_df = eval_df[selected_columns]
 
@@ -42,29 +46,27 @@ def split_data(annotations, text_columnname : str, categories_columnname : str):
 
     return train_df, eval_df
 
-## create and save model
-def train_model(train_df, evaluation_df, modelname = "my_model"):
-    # toggle logging
-    #from transformers.utils import logging
-    #logging.set_verbosity_info()
 
-    #TOKENIZERS_PARALLELISM=True
+## create and save model
+def train_model(train_df, evaluation_df=None, modelname="my_model"):
+    # toggle logging
+    # from transformers.utils import logging
+    # logging.set_verbosity_info()
+
+    # TOKENIZERS_PARALLELISM=True
 
     # Optional model configuration
     model_args = ClassificationArgs(
-                                    num_train_epochs=4,
-                                    train_batch_size=64,
-                                    
-                                    evaluate_during_training=True,
-                                    evaluate_during_training_verbose=True,
-                                    evaluate_during_training_steps=100,
-
-                                    overwrite_output_dir= True,
-                                    use_multiprocessing = False,
-                                    use_multiprocessing_for_evaluation = False
-
-                                    )
-    '''
+        num_train_epochs=6,
+        train_batch_size=32,
+        evaluate_during_training=False,
+        evaluate_during_training_verbose=False,
+        evaluate_during_training_steps=100,
+        overwrite_output_dir=True,
+        use_multiprocessing=False,
+        use_multiprocessing_for_evaluation=False,
+    )
+    """
     self.args = {
        'model_type':  'roberta',
        'model_name': 'roberta-base',
@@ -91,34 +93,34 @@ def train_model(train_df, evaluation_df, modelname = "my_model"):
        'overwrite_output_dir': False,
        'reprocess_input_data': False,
     }
-    '''
+    """
 
     model = ClassificationModel(
-                                'distilbert',
-                                'distilbert-base-uncased',
-                                use_cuda=False,
-                                args=model_args,
-                                weight=[0.625, 2.5]
-                                )
-    #--> set cuda to True, if available
+        "distilbert",
+        "distilbert-base-uncased",
+        use_cuda=False,
+        args=model_args,
+        weight=[0.625, 2.5],
+    )
+    # --> set cuda to True, if available
 
     # start training#
-    model.train_model(train_df, eval_df=evaluation_df)
+    model.train_model(train_df)
 
-    #save model
+    # save model
     model.model.save_pretrained(modelname)
     model.tokenizer.save_pretrained(modelname)
-    model.config.save_pretrained(f'{modelname}/')
+    model.config.save_pretrained(f"{modelname}/")
 
 
 ### evaluate model
-#evaluate model
+# evaluate model
 def eval_model(eval_df, modelname):
     # reload model (if needed)
-    model_args = ClassificationArgs(
-                                    use_multiprocessing_for_evaluation = False
-                                    )
-    model = ClassificationModel('distilbert', modelname, use_cuda=False, args=model_args)
+    model_args = ClassificationArgs(use_multiprocessing_for_evaluation=False)
+    model = ClassificationModel(
+        "distilbert", modelname, use_cuda=False, args=model_args
+    )
 
     # start evaluation
     print("Starting Evaluation:")
@@ -129,15 +131,15 @@ def eval_model(eval_df, modelname):
     for i in result:
         print(i, result[i])
 
-    #tp = true positives
-    #tn = true negatives
-    #fp = false positives
-    #fn = false negaives
-    #auroc = precision
-    #auprc = average precison
-        
+    # tp = true positives
+    # tn = true negatives
+    # fp = false positives
+    # fn = false negaives
+    # auroc = precision
+    # auprc = average precison
+
     # print all wrong preductions (if wanted)
-    #for n, dic in enumerate(wrong_predictions):
+    # for n, dic in enumerate(wrong_predictions):
     #    print("Annotation Label:", dic.label)
     #    print(dic.text_a)
     #    print(10*'+')
--- BERT-Classification/classify_tweets.py
+++ BERT-Classification/classify_tweets.py
@@ -1,26 +1,28 @@
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
+
 # https://pypi.org/project/simpletransformers/ <- source evaluation tipps
 from scipy.special import softmax
-#https://github.com/ThilinaRajapakse/simpletransformers/issues/30 
+
+# https://github.com/ThilinaRajapakse/simpletransformers/issues/30
 import pandas as pd
 from sys import argv
 from sys import stderr
 import os
 
 from classification_functions import split_data, train_model, eval_model
-'''
+
+"""
 requirements:
 pip install simpletransformers
 pip install scipy
 pip install torch
 
-'''
+"""
 
 if __name__ == "__main__":
     # Split Annotation Dataset
     dataset = pd.read_csv("annotation_data/annotate_1000_final.tsv", sep="\t")
-    dataset.rename(columns={'comment': 'text', 
-                            'category': 'labels'}, inplace=True)
+    dataset.rename(columns={"comment": "text", "category": "labels"}, inplace=True)
     dataset = dataset[["text", "labels"]]
     dataset = dataset[dataset["labels"] != -1]
     print(dataset["labels"].value_counts())
@@ -29,25 +31,17 @@ if __name__ == "__main__":
 
     match input("train, eval or apply?"):
         case "train":
-            train_model(train_df, evaluation_df = eval_df, modelname="my_model")
+            train_model(train_df, modelname="my_model")
+            eval_model(eval_df, modelname="my_model")
 
         case "eval":
             eval_model(eval_df, "my_model")
 
-        #case "apply":
+        # case "apply":
         #    break
 
-        
 
-
-
-
-
-
-
-
-
-'''
+"""
 calc weights of class
 
     n_samples=  43400,  n_classes= 2(0&1), n_sample0= 42617, n_samples1= 783
@@ -62,4 +56,4 @@ w0 = 940/(1504) = 0,625
 w1 = 940/(376) = 2,5
 
 
-'''
+"""
